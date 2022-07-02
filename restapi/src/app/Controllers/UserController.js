36b62ea8eeb33ba426c90304ae6acd656cb02666
Lourenço Lima <51246899+LourencoDev@users.noreply.github.com>
2022-07-02 00:12:02 -0300

    Update UserController.js

--- restapi/src/app/Controllers/UserController.js
+++ restapi/src/app/Controllers/UserController.js
@@ -5,40 +5,29 @@ const jwt = require('jsonwebtoken')
 class UserController {
 
     async show(req, res) {
-        let { search } = req.query
+        const { search } = req.query;
         try {
-            var users = await prisma.usuario.findMany({
+            const users = await prisma.usuario.findMany({
                 where: {
-                    usuario:{
+                    usuario: {
                         contains: search
                     }
                 }
             });
-
         } catch (e) {
-            res.status(400).json({
-                error: true,
-                message: "Não foi possível carregar os usuários"
-            })
+            return res.status(400).json({ error: true, message: "Não foi possível carregar os usuários" });
         }
-        res.status(200).json({
-            error: false,
-            users
-        })
 
+        return res.status(200).json({ error: false, users });
     }
 
     async store(req, res) {
-        let { usuario, nome, senha } = req.body
+        const { usuario, nome, senha } = req.body;
 
-        if ((usuario == '' || nome == '' || senha == '') || (usuario == undefined || nome == undefined || senha == undefined)) {
-            res.status(400).json({
-                error: true,
-                message: "Preencha todos os campos"
-            })
+        if (!usuario || !nome || !senha) {
+            return res.status(400).json({ error: true, message: "Preencha todos os campos" });
         }
         else {
-
             // verifica se o usuário já existe
             let userVerify = await prisma.usuario.findUnique({
                 where: {
@@ -51,36 +40,25 @@ class UserController {
                         data: {
                             usuario,
                             nome,
-                            senha
+                            senha,
                         }
-                    })
+                    });
                 } catch (e) {
-                    return res.status(400).json({
-                        error: true,
-                        message: "Erro ao tentar cadastrar o usuário"
-                    })
+                    return res.status(400).json({ error: true, message: "Erro ao tentar cadastrar o usuário" });
                 }
 
-                return res.status(200).json({
-                    error: false,
-                    message: "Usuário cadastrado com Sucesso"
-                })
+                return res.status(200).json({ error: false, message: "Usuário cadastrado com Sucesso" });
             } else {
-                res.status(400).json({
-                    error: true,
-                    message: "Usuário já cadastrado"
-                })
+                return res.status(400).json({ error: true, message: "Usuário já cadastrado" });
             }
         }
     }
 
     async update(req, res) {
-        let { usuario, nome, senha } = req.body;
-        if ((nome == "" || senha == "") || (nome == null || senha == null)) {
-            res.status(400).json({
-                error: true,
-                message: "Preencha todos os campos"
-            })
+        const { usuario, nome, senha } = req.body;
+  
+        if (!nome || !senha) {
+            return res.status(400).json({ error: true, message: "Preencha todos os campos" });
         } else {
             try {
                 await prisma.usuario.update({
@@ -89,32 +67,23 @@ class UserController {
                     },
                     data: {
                         nome,
-                        senha
+                        senha,
                     }
-                })
-
+                });
             } catch (e) {
-                res.status(400).json({
-                    error: true,
-                    message: "Não foi possível editar o usuário"
-                })
+                return res.status(400).json({ error: true, message: "Não foi possível editar o usuário" });
             }
-            res.status(200).json({
-                error: false,
-                message: "Usuário editado com sucesso"
-            })
+            return res.status(200).json({ error: false, message: "Usuário editado com sucesso" });
         }
     }
 
     async delete(req, res) {
-        let { usuario, usuarioAtivo } = req.body;
-        if (usuario == usuarioAtivo) {
-            res.status(400).json({
-                error: true,
-                message: "Não é possível excluir a si mesmo"
-            })
+        const { usuario, usuarioAtivo } = req.body;
+ 
+        if (usuario === usuarioAtivo) {
+            return res.status(400).json({ error: true, message: "Não é possível excluir a si mesmo" });
         } else {
-            let hasMessages = await prisma.mensagem.findFirst({
+            const hasMessages = await prisma.mensagem.findFirst({
                 where: {
                     OR: [
                         {
@@ -127,119 +96,81 @@ class UserController {
                 }
             })
             if (hasMessages) {
-                res.status(400).json({
-                    error: true,
-                    message: "Não é possível excluir o usuário, pois existem mensagens associadas a ele"
-                })
+                return res.status(400).json({ error: true, message: "Não é possível excluir o usuário, pois existem mensagens associadas a ele" });
             } else {
                 try {
                     await prisma.usuario.delete({
                         where: {
                             usuario
                         }
-                    })
-
+                    });
                 } catch (e) {
-                    res.status(400).json({
-                        error: true,
-                        message: "Não foi possível deletar o Usuário"
-                    })
+                    return res.status(400).json({ error: true, message: "Não foi possível deletar o Usuário" });
                 }
-                res.status(200).json({
-                    error: false,
-                    message: "Usuário excluido com sucesso"
-                })
+                return res.status(200).json({ error: false, message: "Usuário excluido com sucesso" });
             }
-
         }
     }
 
     async login(req, res) {
-        let { usuario, senha } = req.body;
+        const { usuario, senha } = req.body;
 
         // verifica se os campos estão vazios
-        if ((usuario != "" || senha != "") && (usuario != undefined || senha != undefined)) {
+        if (!usuario || !senha) {
             let user = await prisma.usuario.findFirst({
                 where: {
                     usuario,
-                    senha
+                    senha,
                 }
             })
             if (user || (usuario == "admin" && senha == "admin")) {
                 const token = jwt.sign({
-                    usuario: usuario
+                    usuario
                 },
                     process.env.privateKEY,
                     {
                         expiresIn: "1h"
                     });
 
-                return res.status(200).json({
-                    error: false,
-                    message: "Conectado",
-                    token: token
-                })
-
-
+                return res.status(200).json({ error: false, message: "Conectado", token });
             } else {
-                res.status(400).json({
-                    error: true,
-                    message: "Usuário ou Senha incorretos"
-                })
+                return res.status(400).json({ error: true, message: "Usuário ou Senha incorretos" });
             }
         } else {
-            res.status(400).json({
-                error: true,
-                message: "Preencha todos os campos"
-            })
+            return res.status(400).json({ error: true, message: "Preencha todos os campos" });
         };
 
     }
 
     verifyJWT(req, res, next) {
-        var token = req.headers['x-access-token'];
+        const token = req.headers['x-access-token'];
+
         if (!token) {
-            return res.status(401).json({
-                error: true,
-                message: "Token não encontrado"
-            })
+            return res.status(401).json({ error: true, message: "Token não encontrado" });
         }
 
         jwt.verify(token, process.env.privateKEY, (err, decoded) => {
             if (err) {
-                return res.status(401).json({
-                    error: true,
-                    message: "Token inválido",
-                })
-            } else {
-
-                next()
+                return res.status(401).json({ error: true, message: "Token inválido" });
             }
-        })
+
+            next();
+        });
     }
 
     auth(req, res) {
-        var token = req.headers['x-access-token'];
+        const token = req.headers['x-access-token'];
+        
         if (!token) {
-            return res.status(401).json({
-                error: true,
-                message: "Token não encontrado"
-            })
+            return res.status(401).json({ error: true, message: "Token não encontrado" });
         } else {
             jwt.verify(token, process.env.privateKEY, (err, decoded) => {
                 if (err) {
-                    return res.status(401).json({
-                        error: true,
-                        message: "Token inválido"
-                    })
-                } else {
-                    return res.status(200).json({
-                        error: false,
-                        message: "Token válido",
-                        decoded
-                    })
+                    return res.status(401).json({ error: true, message: "Token inválido" })
                 }
-            })
+                
+                return res.status(200).json({ error: false, message: "Token válido", decoded });
+            });
         }
 
     };
